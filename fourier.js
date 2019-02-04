2ce17a4ffcf4af6a35b1c2b2804a2fd76a039955
akukettunen <45817413+akukettunen@users.noreply.github.com>
2019-02-04 02:21:33 +0200

    Update fourier.js

--- fourier.js
+++ fourier.js
@@ -9,34 +9,50 @@ const circlclr = 255;
 const width = 700;
 const height = 600;
 let balls = 1;
-teksti = "Click screen to add precision";
-teksti2 = "Refresh page to start over";
+teksti = "Press 'a' to add a iteration";
+teksti2 = "Press 'r' to set iterations to 0";
+teksti21 = "Hold 's' to add iterations fast";
+teksti3 = "Function, where L is the frequency of the largest circle:";
+teksti4 = "Change time"
 
 function setup() {
 	canvas = createCanvas(width, height);
 	angle = 0;
 	pisteet = [];
 	circlsetup();
+    rSlider = createSlider(20, 250, 30, 10);
+  	rSlider.position(530, 30);
+    button = createButton("What is this?");
+  	button.position(610, 92);
+    button.mousePressed(ohjeet);
 }
 
 
 function draw() {
+    
 	canvas.background(bgclr);
+    
+    fill(255);
+    rect(0,0, width, height / 8);
+    
 	noFill();
 
 	add_circles_maybe();
 
 	n = "n: " + balls;
     
+    fill(0);
 	text(n, 10, 20);
 	text(teksti, 10, 35);
 	text(teksti2, 10, 50);
+    text(teksti21, 10, 65);
+    //text(teksti3, 10, 70);
+    text(teksti4, 530, 20);
 
 	for (i = 0; i < balls; i++) {
 		lista[i].draw();
 	}
 
-	angle -= 1 / 35;
 
 	viivay = lista[balls - 1].get_endpoint('y');
 	viivax = lista[balls - 1].get_endpoint('x');
@@ -45,6 +61,8 @@ function draw() {
 	pisteet.push(viivay);
 
 	line(viivax, viivay, 400, viivay);
+    
+    angle -= 1 / rSlider.value();
 
 
 	for (var i = 0; i < pisteet.length - 4; i += 4) {
@@ -54,17 +72,26 @@ function draw() {
 		pisteet[i + 2] += 1 / 2;
 	}
 
-	while (pisteet.length > 400) {
+	while (pisteet.length > 1000) {
 		pisteet.shift();
 	}
-
+    
+    if (keyIsPressed === true) {
+        if (key === 'r') {
+        	balls = 1;
+        } else if (key === 's'){
+            if (balls < 300) {
+            balls += 1;
+            }
+        }
+    }
 }
 
 class Circle {
 
 	constructor(Circl, n) {
 		this.xcoor = width / 5;
-		this.ycoor = height / 2;
+		this.ycoor = height / 2 + height / 10;
 		this.n = n;
 		this.r = 4 / (3 * this.n) * 100;
 		this.angle = 0;
@@ -107,6 +134,13 @@ function add_circles_maybe() {
 	}
 }
 
-function mouseClicked() {
-	balls += 1;
+function keyReleased() {
+    if(key === 'a') {
+        balls += 1
+    }
 }
+function ohjeet() {
+    let url = "http://bilimneguzellan.net/en/purrier-series-meow-and-making-images-speak/"
+    window.open(url);
+}
+
